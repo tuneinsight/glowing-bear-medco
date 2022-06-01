4fc581afff97e8252e5ca525926ff84d05f7ac39
Jimmy DJABALI <hello@jimmydjabali.com>
2022-06-01 13:33:37 +0200

    Add 403 & generic use-case in getNetwork() (#29)
    * Add 403 & generic use-case in getNetwork()
    
    * Handle generic error for initial tree loading
    
    * Logout works
    
    * Changed minimum roles

--- src/app/services/tree-node.service.ts
+++ src/app/services/tree-node.service.ts
@@ -91,8 +91,10 @@ export class TreeNodeService {
               resolve();
             },
             (err) => {
-              ErrorHelper.handleError('Error during initial tree loading', err);
-              this._isLoading = false;
+              console.error(err);
+              let errMessage = 'Undefined error when checking network.';
+              alert(`${errMessage} Please contact an administrator. You will now be logged out.`);
+              this.keycloakService.logout();
               reject(err);
             }
           );
