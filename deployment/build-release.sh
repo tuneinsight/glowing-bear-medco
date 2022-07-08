5a4426705537c376e33a43df25a0f1db4f0ec370
Jimmy DJABALI <hello@jimmydjabali.com>
2022-07-08 15:46:39 +0200

    Replace token is not exists

--- deployment/build-release.sh
+++ deployment/build-release.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-USER_GROUP="$(id -u):$(id -g)" docker-compose build --no-cache --build-arg TI_ACCESS_TOKEN=$(cat /run/secrets/TI_ACCESS_TOKEN || echo $TI_ACCESS_TOKEN) glowing-bear-medco
+USER_GROUP="$(id -u):$(id -g)" docker-compose build --no-cache --build-arg TI_ACCESS_TOKEN=$TI_ACCESS_TOKEN glowing-bear-medco
